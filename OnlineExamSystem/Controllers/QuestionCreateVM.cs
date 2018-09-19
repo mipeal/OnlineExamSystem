ee85f1998ccb0b4f449b819cc1df566f8d9c586e
Tareq Rahman <tareq.bes@gmail.com>
2018-09-19 17:28:44 +0600

    QuestionEntry

---
+++ OnlineExamSystem/Controllers/QuestionCreateVM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+using Models;
+
+namespace OnlineExamSystem.Controllers
+{
+    public class QuestionCreateVM
+    {
+        public QuestionCreateVM()
+        {
+        }
+
+        public List<SelectListItem> OrganizationSelectListItems { get; set; }
+        public List<SelectListItem> CourseSelectListItems { get; set; }
+        public List<SelectListItem> ExamSelectListItems { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+        [ForeignKey("CourseId")]
+        public virtual Course Course { get; set; }
+
+        public string Exam { get; set; }
+        public int ExamId { get; set; }
+       
+    }
+}
